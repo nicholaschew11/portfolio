804a8b9b42765b626dba52657b56e408f4b6ac29
n <chew.nicky@gmail.com>
2023-08-01 14:18:30 -0400

    a

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,9 @@ if (typeof window !== 'undefined') {
 function Website({ Component, pageProps, router }) {
   return (
     <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+    />
     <Script
       id='google-analytics'
       strategy="afterInteractive"
@@ -20,11 +22,11 @@ function Website({ Component, pageProps, router }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', process.env.MEASUREMENT_ID, {
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
             page_path: window.location.pathname,
           });
         `,
-        }}
+      }}
     />
     <Chakra cookies={pageProps.cookies}>
       <Layout router={router}>
